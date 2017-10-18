a036f6b00fe18aebd6bae153222af93d722a0ac3
AnastasiaBarybinaS <ab@aic.ru>
2017-10-18 16:08:48 +0300

    Basic Methods

--- AB_calc.py
+++ AB_calc.py
@@ -85,6 +85,22 @@ def ANOVA(data):
     elif p > 0.05:
         return 'Groups have the same median F = %f p-value = %f' % (p,F)
 
+
+#TWO WAY ANOVA PROTOTYPE
+def anova_interaction(data):
+    # Get the data
+
+    # --- >>> START stats <<< ---
+    # Determine the ANOVA with interaction
+    formula = 'values ~ C(hip)'
+    lm = ols(formula, data).fit()
+    anovaResults = anova_lm(lm)
+    # --- >>> STOP stats <<< ---
+    print data.columns
+    print(anovaResults)
+
+    return anovaResults['F'][0]
+
 def WILCOX(data):
     p = stat.wilcoxon(*data)
     print 'Standartone-way wilcox test'
@@ -163,7 +179,6 @@ def divider(n, mas):
     print ANOVA(dlist)
     print kruskalWallis(dlist)
     print simpleStatistics(dlist,n,mas)
-    return
 
 print divider(brnch["num"], brnch["mass"])
 
@@ -181,21 +196,95 @@ def visual(n, mas):
     for param in range(0, n):
         for i in range(0, observes):
             nd.loc[i+observes*param, 'hip'] = str(mas[param])
-            nd.loc[i+observes*param, 'values']= total.loc[i,str(mas[param])];
+            nd.loc[i+observes*param, 'values']= total.loc[i,str(mas[param])]
     #nd.loc[:,'hip'].dtype = '|S8'
     #nd.loc[:, 'values'].dtype = '<i4'
+    nd[['values']] = nd[['values']].apply(pd.to_numeric)
+    nd[['hip']].to_string()
+
+    trace0 = Box(
+        y=nd.loc[:, 'values'],
+        x=nd.loc[:, 'hip'],
+        boxpoints='all',
+        jitter=0.3,
+        pointpos=-1.8,
+        name=['kale', 'gate'],
+        marker=dict(
+            color=['#3D9970', '#453D32']
+        ),
+        fillcolor=['#3D9970', '#453D32']
+    )
+    data = [trace0]
+    layout = Layout(
+        yaxis=dict(
+            title='normalized moisture',
+            zeroline=False
+        ),
+        boxmode='group'
+    )
+    # fig = go.Figure(data=data, layout=layout)
+    # py.plot(fig)
+    fig = Figure(data=data, layout=layout)
+    plot(fig)
+
+    anova_interaction(nd)
 
     nd2 = nd.to_records()
-    nd2[(u'hip')].astype(str)
-    nd2[u'values'].astype(int)
     #res2 = MultiComparison(nd2['values'],nd2['hip'])
-    #res2 = pairwise_tukeyhsd(nd2[u'values'], nd2[u'hip'])
-    print nd2.dtype
+    #res2 = res2.tukeyhsd()
+    res2 = pairwise_tukeyhsd(nd2[u'values'], nd2[u'hip'])
+    print res2
 
 print visual(brnch["num"], brnch["mass"])
 
 
+funnel_df = pd.DataFrame(data=np.array([["Шаг 1", 100, 100, 100, 100],["Шаг 2", 70, 70, 80, 80], ["Шаг 3", 60, 73, 40,66],
+                                        ["Шаг 4", 40, 57, 20, 50]]), columns=["Steps","A_first","A_prev","B_first","B_prev"])
+print funnel_df
 
+
+trace1 = Bar(
+        x=funnel_df.loc[:,'Steps'],
+        y=funnel_df.loc[:,"A_first"],
+        text=funnel_df.loc[:,"A_first"]+"%",
+        textposition = 'auto',
+        textfont={
+                "size": 22,
+                "color": "white"
+            },
+        marker=dict(
+                color=['rgba(101,120,151,0.8)', 'rgba(88,127,188,0.8)',
+                       'rgba(68,163,215,0.8)', 'rgba(43,228,247,0.8)',
+                       'rgba(90,86,82,1)']),
+        name='От первого шага'
+)
+trace2 = Scatter(
+    x=funnel_df.loc[:, 'Steps'],
+    y=funnel_df.loc[:, "A_prev"],
+    fill='tonexty',
+    mode='lines+text+percent',
+    line=dict(
+        color='rgba(143,228,247,0.3)',
+    ),
+    text=funnel_df.loc[:,"A_prev"]+'%',
+    textposition = 'top',
+    textfont={
+                "size": 22
+            },
+    name='От предыдущего шага'
+)
+
+data = [trace2, trace1]
+layout = Layout(
+    barmode='group'
+)
+
+fig = Figure(data=data, layout=layout)
+plot(fig, filename='grouped-bar')
+
+
+
+#plot(data, filename='basic-bar')
 #for i in list(total.columns.values):
 #names of columns
 
